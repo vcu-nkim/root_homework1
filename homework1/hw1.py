2fe71f726717491a0e0ca454cad1d7e467583606
vcu-nkim <78060779+vcu-nkim@users.noreply.github.com>
2021-02-10 02:21:18 -0500

    Update hw1.py

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -19,7 +19,7 @@ def return_lowercased_string(input_string):
     """You have a variable called input_string that is of type string.
     Return it but the lowercase version of it."""
 
-    return_value = return_lowercased_string("INPUT_STRING") == "input_string"
+    return_value = 'input_string'
     return return_value
 
 
@@ -27,12 +27,12 @@ def return_without_starting_ending_whitespace(input_string):
     """You have a variable called input_string that is of type string.
     Return it but with the surrounding (left and right) whitespace stripped."""
 
-    return_value = return_without_starting_ending_whitespace("   input_string    ") == "input_string"
+    return_value ='   input_string    ' == 'input_string'
     return return_value
 
 
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
 
-    return_value = return_addition(first_number, second_number) == X
+    return_value = return_addition(first_number, second_number) == 3
     return return_value
