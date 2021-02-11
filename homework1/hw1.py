e22e337e3de5d69c4a7dd30fd5c3a594fddf7481
vcu-nkim <78060779+vcu-nkim@users.noreply.github.com>
2021-02-10 19:32:57 -0500

    Update hw1.py

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -19,8 +19,8 @@ def return_lowercased_string(input_string):
     """You have a variable called input_string that is of type string.
     Return it but the lowercase version of it."""
 
-    return_value = return_lowercased_string("INPUT_STRING") == "input_string"
-    return return_value
+    string = "INPUT_STRING"
+    print(string.lower())
 
 
 def return_without_starting_ending_whitespace(input_string):
