953fd0d40150392ba39289e4b5037c47299415eb
vcu-nkim <78060779+vcu-nkim@users.noreply.github.com>
2021-02-12 21:18:30 -0500

    Update hw1.py

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -1,16 +1,19 @@
 """ This set of functions should return the appropriate data type"""
 
+
 def return_number_3():
     """ This function should return an integer with the value of 3"""
 
     return_value = 3
     return return_value
 
+
 def return_string_vcu():
     """ This function should return a string with the lowercase value of vcu"""
 
     return_value = 'vcu'
     return return_value
+
 
 def return_lowercased_string(input_string):
     """You have a variable called input_string that is of type string.
@@ -19,12 +22,14 @@ def return_lowercased_string(input_string):
     string = input_string.lower()
     return string
 
+
 def return_without_starting_ending_whitespace(input_string):
     """You have a variable called input_string that is of type string.
     Return it but with the surrounding (left and right) whitespace stripped."""
 
     string = input_string.strip()
     return string
+
 
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
